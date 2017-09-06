9cd6e7c6027a9465df78a4e4775953ee75e10eab
Jing Li <lijing@example.com>
2017-09-06 12:08:39 +0800

    normal hsmp log

--- README.md
+++ README.md
@@ -5,3 +5,22 @@ build
 -----
 
 docker-compose up --build
+
+
+General operation
+=================
+
+elasticsearch
+-------------
+
+1. cat api
+
+        # show indices
+        curl http://localhost:9200/_cat/indices?v
+
+2. index api
+
+        # get index mapping
+        curl http://localhost:9200/logstash-20170906/_mapping?pretty
+        # delete indices
+        curl -XDELETE http://localhost:9200/logstash-*
--- pipe/pipe.py
+++ pipe/pipe.py
@@ -12,21 +12,18 @@ def current_index():
     today = datetime.datetime.now().strftime("%Y%m%d")
     return 'logstash-{0}'.format(today)
 
-
 redis = redis.Redis(host='localhost', port=6379)
-es = elasticsearch.Elasticsearch(["localhost"], sniff_on_start=True,
-                   sniff_on_connection_fail=True,
-                   sniffer_timeout=60)
+#es = elasticsearch.Elasticsearch(["localhost"], sniff_on_start=True,
+#                   sniff_on_connection_fail=True,
+#                   sniffer_timeout=60)
+es = elasticsearch.Elasticsearch()
 while True:
     x = redis.lpop('logstash')
     if not x:
-        print 'continue'
-        time.sleep(1)
+        time.sleep(0.1)
         continue
     d = json.loads(x) # is a dict
-    print d
-    time.sleep(1)
+    time.sleep(0.1)
     idx = current_index()
     r = es.index(index=idx, doc_type='log', body=d)
-
-    print r
+    print type(r), r
